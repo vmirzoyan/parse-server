3d66f2247ca68a927c9aba1398131f2817b4a7a2
Viktor Mirzoyan <viktormirzoyan@gmail.com>
2020-05-13 17:02:39 -0400

    Update AzurePushAdapter.js

--- src/azure-push/AzurePushAdapter.js
+++ src/azure-push/AzurePushAdapter.js
@@ -19,6 +19,8 @@ module.exports = function AzurePushAdapter(pushConfig) {
   let api = {
     getValidPushTypes: () => ['ios', 'android', 'winrt'],
     send: (data, installations) => {
+      let installCount = (installations || []).length;
+      console.log( `Sending to ${installCount} installations`)
       let deviceMap = classifyInstallations(installations, api.getValidPushTypes());
       let sendPromises = [];
       for (let pushType in deviceMap) {
